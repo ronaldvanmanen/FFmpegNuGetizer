1e98c0219307e8d586b1f9dbaa7416b5f9c204ec
Ronald van Manen <rvanmanen@gmail.com>
2024-01-17 15:01:29 +0100

    Add target to build port package.

--- build/Build.cs
+++ build/Build.cs
@@ -1,37 +1,100 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Clean);
+    public static int Main () => Execute<Build>(x => x.BuildPortPackage);
+
+    [Parameter()]
+    readonly string VcpkgTriplet;
+
+    [Parameter()]
+    readonly string VcpkgFeature;
+
+    [Parameter()]
+    readonly string[] VcpkgBinarySources = Array.Empty<string>();
+
+    [Parameter()]
+    readonly bool VcpkgDebug;
 
     AbsolutePath ArtifactsRootDirectory => RootDirectory / "artifacts";
+
+    AbsolutePath VcpkgArtifactsRootDirectory => ArtifactsRootDirectory / "vcpkg";
+
+    AbsolutePath VcpkgBuildtreesRootDirectory => VcpkgArtifactsRootDirectory / VcpkgFeature / VcpkgTriplet / "buildtrees";
+
+    AbsolutePath VcpkgDownloadsRootDirectory => VcpkgArtifactsRootDirectory / VcpkgFeature / VcpkgTriplet / "downloads";
+
+    AbsolutePath VcpkgInstallRootDirectory => VcpkgArtifactsRootDirectory / VcpkgFeature / VcpkgTriplet / "installed";
+
+    AbsolutePath VcpkgPackagesRootDirectory => VcpkgArtifactsRootDirectory / VcpkgFeature / VcpkgTriplet / "packages";
 
     [LocalPath(windowsPath: "vcpkg/bootstrap-vcpkg.bat", unixPath: "vcpkg/bootstrap.sh")]
     readonly Tool BootstrapVcpkg;
 
+    [LocalPath(windowsPath: "vcpkg/vcpkg.exe", unixPath: "vcpkg/vcpkg")]
+    readonly Tool Vcpkg;
+
+    [UsedImplicitly]
     Target Clean => _ => _
         .Executes(() =>
         {
             ArtifactsRootDirectory.CreateOrCleanDirectory();
         });
 
-    [UsedImplicitly]
     Target SetupVcpkg => _ => _
+        .Unlisted()
         .Executes(() =>
         {
             BootstrapVcpkg("-disableMetrics");
         });
     
+    Target BuildPortPackage => _ => _
+        .DependsOn(SetupVcpkg)
+        .Requires(() => VcpkgTriplet)
+        .Requires(() => VcpkgFeature)
+        .Executes(() =>
+        {
+            VcpkgBuildtreesRootDirectory.CreateDirectory();
+            VcpkgDownloadsRootDirectory.CreateDirectory();
+            VcpkgInstallRootDirectory.CreateDirectory();
+            VcpkgPackagesRootDirectory.CreateDirectory();
+
+            var argumentList = new List<string>
+            {
+                $"install",
+                $"--triplet={VcpkgTriplet}",
+                $"--downloads-root={VcpkgDownloadsRootDirectory}",
+                $"--x-buildtrees-root={VcpkgBuildtreesRootDirectory}",
+                $"--x-install-root={VcpkgInstallRootDirectory}",
+                $"--x-packages-root={VcpkgPackagesRootDirectory}",
+                $"--x-no-default-features",
+                $"--x-feature={VcpkgFeature}",
+                $"--clean-after-build",
+                $"--disable-metrics",
+            };
+
+            argumentList.AddRange(
+                VcpkgBinarySources.Select(vcpkgBinarySource => $"--binarysource={vcpkgBinarySource}")
+            );
+
+            if (VcpkgDebug)
+            {
+                argumentList.Add("--debug");
+            }
+
+            // NOTE: ArgumentStringHandler assumes our arguments must be double qouted because we
+            // pass our arguments as a single string. To prevent this from happening we explicity
+            // create an ArgumentStringHandler and append our arguments as a string literal.
+            var argumentString = string.Join(' ', argumentList);
+            ArgumentStringHandler arguments = "";
+            arguments.AppendLiteral(argumentString);
+
+            Vcpkg(arguments);
+        });
 }
